b0f4856ebb7e446945b1e78e519e954be970c821
Den4iqov <57962531+yuriy1908@users.noreply.github.com>
2024-04-07 19:23:47 +0500

    Update Figure.cs
    v1.1.0

--- tetris/Figure.cs
+++ tetris/Figure.cs
@@ -11,7 +11,6 @@ namespace tetris
     {
         public int ox;
         public int oy;
-        public char[,] pic = new char[4, 4];
         public int[] heigts;
         public int[,] xs;
         public int[,] ys;
@@ -23,20 +22,12 @@ namespace tetris
             this.ox = ox;
             this.oy = oy;
         }
-        public void Clear()
-        {
-            for (int i = 0; i < 4; i++)
-                for (int j = 0; j < 4; j++)
-                    pic[i, j] = (char)0;
-        }
     }
     public class Stick: Shape
     {
         public Stick(int ox, int oy)
             :base(ox, oy)
         {
-            for (int i = 0; i < 4; i++)
-                this.pic[0, i] = symbol;
             this.heigts = new int[] { 1, 1, 1, 1 };
             this.xs = new int[,] { { 0, 1, 2, 3 }, { 0, 0, 0, 0 } };
             this.ys = new int[,] { { 0, 0, 0, 0 }, { 0, 1, 2, 3 } };
@@ -44,17 +35,12 @@ namespace tetris
         public override void Rotate()
         {
             index = (index + 1) % 2;
-            Clear();
             if (index == 0)
             {
-                for (int i = 0; i < 4; i++)
-                    this.pic[0, i] = symbol;
                 this.heigts = new int[] { 1, 1, 1, 1 };
             }
             else
             {
-                for (int i = 0; i < 4; i++)
-                    this.pic[i, 0] = symbol;
                 this.heigts = new int[] { 4 };
             }
         }
@@ -64,9 +50,6 @@ namespace tetris
         public Square(int ox, int oy)
             : base(ox, oy)
         {
-            for (int i = 0; i < 2; i++)
-                for (int j = 0; j < 2; j++)
-                    this.pic[i, j] = symbol;
             this.heigts = new int[] { 2, 2 };
             this.xs = new int[,] { { 0, 0, 1, 1 } };
             this.ys = new int[,] { { 0, 1, 0, 1 } };
@@ -78,9 +61,6 @@ namespace tetris
         public Horse1(int ox, int oy)
             : base(ox, oy)
         {
-            this.pic[0, 0] = symbol;
-            for (int i = 0; i < 3; i++)
-                this.pic[i, 1] = symbol;
             this.heigts = new int[] { 1, 3 };
             this.xs = new int[,] { { 0, 1, 1, 1 }, { 2, 0, 1, 2}, { 0, 0, 0, 1}, { 0, 1, 2, 0} };
             this.ys = new int[,] { { 0, 0, 1, 2 }, { 0, 1, 1, 1}, { 0, 1, 2, 2}, { 1, 0, 0, 0 } };
@@ -88,31 +68,18 @@ namespace tetris
         public override void Rotate()
         {
             index = (index + 1) % 4;
-            Clear();
             switch (index)
             {
                 case 0:
-                    this.pic[0, 0] = symbol;
-                    for (int i = 0; i < 3; i++)
-                        this.pic[i, 1] = symbol;
                     this.heigts = new int[] { 1, 3 };
                     break;
                 case 1:
-                    this.pic[0, 2] = symbol;
-                    for (int i = 0; i < 3; i++)
-                        this.pic[1, i] = symbol;
                     this.heigts = new int[] { 2, 2, 2 };
                     break;
                 case 2:
-                    this.pic[2, 1] = symbol;
-                    for (int i = 0; i < 3; i++)
-                        this.pic[i, 0] = symbol;
                     this.heigts = new int[] { 3, 3 };
                     break;
                 default:
-                    this.pic[1, 0] = symbol;
-                    for (int i = 0; i < 3; i++)
-                        this.pic[0, i] = symbol;
                     this.heigts = new int[] { 2, 1, 1 };
                     break;
 
@@ -124,41 +91,25 @@ namespace tetris
         public Horse2(int ox, int oy)
             : base(ox, oy)
         {
-            this.pic[0, 1] = symbol;
-            for (int i = 0; i < 3; i++)
-                this.pic[i, 0] = symbol;
             this.heigts = new int[] { 3, 1 };
             this.xs = new int[,] { { 0, 1, 0, 0 }, { 0, 1, 2, 2 }, { 1, 1, 0, 1}, { 0, 0, 1, 2 } };
             this.ys = new int[,] { { 0, 0, 1, 2 }, { 0, 0, 0, 1 }, { 0, 1, 2, 2}, { 0, 1, 1, 1 } };
         }
         public override void Rotate()
         {
-            Clear();
             index = (index + 1) % 4;
             switch (index)
             {
                 case 0:
-                    this.pic[0, 1] = symbol;
-                    for (int i = 0; i < 3; i++)
-                        this.pic[i, 0] = symbol;
                     this.heigts = new int[] { 3, 1 };
                     break;
                 case 1:
-                    this.pic[1, 2] = symbol;
-                    for (int i = 0; i < 3; i++)
-                        this.pic[0, i] = symbol;
                     this.heigts = new int[] { 1, 1, 2 };
                     break;
                 case 2:
-                    this.pic[2, 0] = symbol;
-                    for (int i = 0; i < 3; i++)
-                        this.pic[i, 1] = symbol;
                     this.heigts = new int[] { 3, 3 };
                     break;
                 default:
-                    this.pic[0, 0] = symbol;
-                    for (int i = 0; i < 3; i++)
-                        this.pic[1, i] = symbol;
                     this.heigts = new int[] { 2, 2, 2 };
                     break;
 
@@ -170,32 +121,19 @@ namespace tetris
         public Zigzag1(int ox, int oy)
             : base(ox, oy)
         {
-            this.pic[0, 0] = symbol;
-            this.pic[0, 1] = symbol;
-            this.pic[1, 1] = symbol;
-            this.pic[1, 2] = symbol;
             this.heigts = new int[] { 1, 2, 2 };
             this.xs = new int[,] { { 0, 1, 1, 2}, { 1, 0, 1, 0 } };
             this.ys = new int[,] { { 0, 0, 1, 1}, { 0, 1, 1, 2 } };
         }
         public override void Rotate()
         {
-            Clear();
             index = (index+1)%2;
             if (index == 0)
             {
-                this.pic[0, 0] = symbol;
-                this.pic[0, 1] = symbol;
-                this.pic[1, 1] = symbol;
-                this.pic[1, 2] = symbol;
                 this.heigts = new int[] { 1, 2, 2 };
             }
             else
             {
-                this.pic[0, 1] = symbol;
-                this.pic[1, 0] = symbol;
-                this.pic[1, 1] = symbol;
-                this.pic[2, 0] = symbol;
                 this.heigts = new int[] { 3, 2 };
             }
         }
@@ -206,32 +144,19 @@ namespace tetris
         public Zigzag2(int ox, int oy)
             : base(ox, oy)
         {
-            this.pic[0, 1] = symbol;
-            this.pic[0, 2] = symbol;
-            this.pic[1, 0] = symbol;
-            this.pic[1, 1] = symbol;
             this.heigts = new int[] { 2, 2, 1 };
             this.xs = new int[,] { { 1, 2, 0, 1 }, { 0, 0, 1, 1 } };
             this.ys = new int[,] { { 0, 0, 1, 1 }, { 0, 1, 1, 2 } };
         }
         public override void Rotate()
         {
-            Clear();
             index = (index + 1) % 2;
             if (index == 0)
             {
-                this.pic[0, 1] = symbol;
-                this.pic[0, 2] = symbol;
-                this.pic[1, 0] = symbol;
-                this.pic[1, 1] = symbol;
                 this.heigts = new int[] { 2, 2, 1 };
             }
             else
             {
-                this.pic[0, 0] = symbol;
-                this.pic[1, 0] = symbol;
-                this.pic[1, 1] = symbol;
-                this.pic[2, 1] = symbol;
                 this.heigts = new int[] { 2, 3 };
             }
         }
@@ -241,46 +166,25 @@ namespace tetris
         public Triangle(int ox, int oy)
             : base(ox, oy)
         {
-            this.pic[0, 1] = symbol;
-            this.pic[1, 0] = symbol;
-            this.pic[1, 1] = symbol;
-            this.pic[1, 2] = symbol;
             this.heigts = new int[] { 2, 2, 2 };
             this.xs = new int[,] { { 1, 0, 1, 2 }, { 0, 0, 1, 0 }, { 0, 1, 2, 1 }, { 1, 0, 1, 1 } };
             this.ys = new int[,] { { 0, 1, 1, 1 }, { 0, 1, 1, 2 }, { 0, 0, 0, 1 }, { 0, 1, 1, 2 } };
         }
         public override void Rotate()
         {
-            Clear();
             index = (index + 1) % 4;
             switch (index)
             {
                 case 0:
-                    this.pic[0, 1] = symbol;
-                    this.pic[1, 0] = symbol;
-                    this.pic[1, 1] = symbol;
-                    this.pic[1, 2] = symbol;
                     this.heigts = new int[] { 2, 2, 2 };
                     break;
                 case 1:
-                    this.pic[0, 0] = symbol;
-                    this.pic[1, 0] = symbol;
-                    this.pic[1, 1] = symbol;
-                    this.pic[2, 0] = symbol;
                     this.heigts = new int[] { 3, 2 };
                     break;
                 case 2:
-                    this.pic[0, 0] = symbol;
-                    this.pic[0, 1] = symbol;
-                    this.pic[0, 2] = symbol;
-                    this.pic[1, 1] = symbol;
                     this.heigts = new int[] { 1, 2, 1 };
                     break;
                 default:
-                    this.pic[0, 1] = symbol;
-                    this.pic[1, 0] = symbol;
-                    this.pic[1, 1] = symbol;
-                    this.pic[2, 1] = symbol;
                     this.heigts = new int[] { 2, 3 };
                     break;
 
